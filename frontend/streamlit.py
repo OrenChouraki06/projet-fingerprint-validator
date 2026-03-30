db6a2f85269ce2af2de69f3443521e9e092cf01f
OrenChouraki06 <laurentchouraki@hotmail.fr>
2026-03-30 09:30:38 +0200

    simpler streamlit interf and some examples

--- frontend/streamlit.py
+++ frontend/streamlit.py
@@ -24,44 +24,38 @@ FINGERPRINT_FASTAPI_URL = "http://localhost:8000"
 # Streamlit application configuration
 # ---------------------------------------------------------------------------------------------
 
-st.set_page_config(page_title="FingerPrint Validator / Fault Detector", page_icon="🔍",
-                   layout="centered")
+st.set_page_config(page_title="FingerPrint Validator / Fault Detector", 
+                   page_icon="🔍", layout="centered", initial_sidebar_state="auto")
 
 st.title("FingerPrint Validator / Fault Detector")
-st.write("Upload an image to analyze its fingerprint and generate a prediction using a FastAPI backend.")  
+st.write("Upload an image to analyze and generate its fingerprint prediction using FastAPI backend.")  
 
-# defintiion of tabs
-tabs = st.tabs(["FingerPrint Image Prediction"])
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg"])
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="✅ Image has been successfully uploaded!", width="content")
 
-# image analysis
-with tabs[0]:
-    st.header("FingerPrint Image Prediction")
+if st.button("Predict FingerPrint", 
+             type="primary", 
+             help="Click to predict the fingerprint class of the uploaded image."):
+    if uploaded_file is None:
+        st.warning("⚠️ Please upload a fingerprint image to predict.")
+    else:
+        with st.spinner("Predicting fingerprint image..."):
+            try:
+                # Convert image to bytes
+                img_bytes = io.BytesIO()
+                image.save(img_bytes, format='JPEG')
+                img_bytes.seek(0)
 
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg"])
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption="✅ Uploaded Image", width="content")
+                # files = {"image": ("image.jpeg", img_bytes, "image/jpeg")}
+                files = {"file": (uploaded_file.name, img_bytes, uploaded_file.type)}   
 
-    if st.button("Predict FingerPrint"):
-        if uploaded_file is None:
-            st.warning("⚠️ Please upload a fingerprint image to predict.")
-        else:
-            with st.spinner("Predicting fingerprint image..."):
-                try:
-                    # Convert image to bytes
-                    img_bytes = io.BytesIO()
-                    image.save(img_bytes, format='JPEG')
-                    img_bytes.seek(0)
+                # Send request to FastAPI backend
+                response = requests.post(f"{FINGERPRINT_FASTAPI_URL}/predict_fingerprint", files=files)
 
-                    # files = {"image": ("image.jpeg", img_bytes, "image/jpeg")}
-                    files = {"file": (uploaded_file.name, img_bytes, uploaded_file.type)}   
+                st.success(f"✅ Fingerprint prediction has been successfully completed!")
+                st.write(response.json())
 
-                    # Send request to FastAPI backend
-                    response = requests.post(f"{FINGERPRINT_FASTAPI_URL}/predict_fingerprint", 
-                                             files=files)
-
-                    st.success(f"✅ Fingerprint prediction completed with status code: {response.status_code}!")
-                    st.write(response.json())
-
-                except Exception as e:
-                    st.error(f"E⚠️ Unable to predict fingerprint image '{uploaded_file.name}': {e}")
+            except Exception as e:
+                st.error(f"E⚠️ Unable to predict the fingerprint image '{uploaded_file.name}': {e}")
