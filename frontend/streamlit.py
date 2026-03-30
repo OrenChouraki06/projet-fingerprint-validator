920822fe7a07167fa0b01ba360154dcbe755a5ed
OrenChouraki06 <laurentchouraki@hotmail.fr>
2026-03-30 10:05:46 +0200

    add slider for confidence threshold

--- frontend/streamlit.py
+++ frontend/streamlit.py
@@ -19,28 +19,42 @@ import requests
 
 # url of the FastAPI backend for the fingerprint validator application
 FINGERPRINT_FASTAPI_URL = "http://localhost:8000"
-FINGERPRINT_CONFIDENCE_THRESHOLD = 75.0
+FINGERPRINT_CONFIDENCE_DEFAULT_THRESHOLD = 0.75
 
 # ---------------------------------------------------------------------------------------------
 # Streamlit application configuration
 # ---------------------------------------------------------------------------------------------
 
-st.set_page_config(page_title="FingerPrint Validator / Fault Detector", 
+def slider_callback():
+    st.session_state.confidence_threshold = st.session_state.slider_value
+
+st.set_page_config(page_title="FingerPrint Validator", 
                    page_icon="🔍", layout="centered", initial_sidebar_state="auto")
 
-st.title("FingerPrint Validator / Fault Detector")
-st.write("Upload an image to analyze and generate its fingerprint prediction using FastAPI backend.")  
+st.title("FingerPrint Validator")
+st.write("Upload an image to generate its fingerprint class prediction.")  
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg"])
+uploaded_file = st.file_uploader("Choose an image...", 
+                                 type=["jpg", "jpeg"],
+                                 help="Click to upload an image file (JPEG format) for fingerprint class prediction. Supported formats: .jpg, .jpeg.")
+
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="✅ Image has been successfully uploaded!", width="content")
 
-if st.button("Predict FingerPrint", 
+confidence_threshold = st.slider("Confidence Threshold (%)", 
+                                 min_value=0.70, 
+                                 max_value=1.0, 
+                                 value=FINGERPRINT_CONFIDENCE_DEFAULT_THRESHOLD, 
+                                 step=0.01,
+                                 format="percent",
+                                 help="Set the confidence threshold for fingerprint class prediction. Predictions with confidence below this threshold will be considered unreliable.")
+
+if st.button("Predict FingerPrint Class", 
              type="primary", 
-             help="Click to predict the fingerprint class of the uploaded image."):
+             help="Click to predict the fingerprint class (i.e. 'thumb', 'index', 'middle', 'ring', or 'little') of the uploaded image."):
     if uploaded_file is None:
-        st.warning("⚠️ Please upload a fingerprint image to predict.")
+        st.warning("⚠️ Please upload an image to predict its fingerprint class.")
     else:
         with st.spinner("Predicting fingerprint image..."):
             try:
@@ -56,15 +70,15 @@ if st.button("Predict FingerPrint",
                 response = requests.post(f"{FINGERPRINT_FASTAPI_URL}/predict_fingerprint", files=files)
 
                 confidence = response.json().get("confidence", 0)
-                if confidence < FINGERPRINT_CONFIDENCE_THRESHOLD:
-                    st.warning(f"⚠️ The confidence level of the prediction is {confidence:.2f}%, which is below the threshold of {FINGERPRINT_CONFIDENCE_THRESHOLD}%.")
+                if confidence < confidence_threshold*100:
+                    st.warning(f"⚠️ The confidence level of the prediction is {confidence:.2f}%, which is below the threshold of {confidence_threshold*100:.2f}%.")
                     st.warning("The prediction may not be reliable.")
                 else:
-                    st.success(f"✅ The confidence level of the prediction is {confidence:.2f}%, which is above the threshold of {FINGERPRINT_CONFIDENCE_THRESHOLD}%.")
+                    st.success(f"✅ The confidence level of the prediction is {confidence:.2f}%, which is above the threshold of {confidence_threshold*100:.2f}%.")
                     st.success("The prediction is likely to be reliable.")
 
-                st.write("Predictions response from FastAPI backend:")
+                st.write("Prediction for all classes:")
                 st.write(response.json())
 
             except Exception as e:
-                st.error(f"E⚠️ Unable to predict the fingerprint image '{uploaded_file.name}': {e}")
+                st.error(f"E⚠️ Unable to predict the fingerprint class from the uploaded image '{uploaded_file.name}': {e}")
