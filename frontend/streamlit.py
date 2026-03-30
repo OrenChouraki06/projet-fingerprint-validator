c6e71ef110ce54fcdecd7f6b196a1b45aa3d8800
OrenChouraki06 <laurentchouraki@hotmail.fr>
2026-03-30 12:19:55 +0200

    About and prediction tabs

--- frontend/streamlit.py
+++ frontend/streamlit.py
@@ -34,51 +34,97 @@ st.set_page_config(page_title="FingerPrint Validator",
 st.title("FingerPrint Validator")
 st.write("Upload an image to generate its fingerprint class prediction.")  
 
-uploaded_file = st.file_uploader("Choose an image...", 
-                                 type=["bmp", "jpg", "jpeg"],
-                                 help="Click to upload an image file (BMP, JPEG format) for fingerprint class prediction.")
+# definition of tabs for the fingerprint application
+tabs = st.tabs(["Image Prediction", "About"])
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="✅ Image has been successfully uploaded!", width="content")
+# ---------------------------------------------------------------------------------------------
+# Tab for the fingerprint image analysis
+# ---------------------------------------------------------------------------------------------
 
-confidence_threshold = st.slider("Confidence Threshold (%)", 
-                                 min_value=0.70, 
-                                 max_value=1.0, 
-                                 value=FINGERPRINT_CONFIDENCE_DEFAULT_THRESHOLD, 
-                                 step=0.01,
-                                 format="percent",
-                                 help="Set the confidence threshold for fingerprint class prediction. Predictions with confidence below this threshold will be considered unreliable.")
+with tabs[0]:
+    st.header("Image Prediction")
+   
+    uploaded_file = st.file_uploader("Choose an image...", 
+                                     type=["bmp", "jpg", "jpeg"],
+                                     help="Click to upload an image file (BMP, JPEG format) for fingerprint class prediction.")
 
-if st.button("Predict FingerPrint Class", 
-             type="primary", 
-             help="Click to predict the fingerprint class (i.e. 'thumb', 'index', 'middle', 'ring', or 'little') of the uploaded image."):
-    if uploaded_file is None:
-        st.warning("⚠️ Please upload an image to predict its fingerprint class.")
-    else:
-        with st.spinner("Predicting fingerprint image..."):
-            try:
-                # Convert image to bytes
-                img_bytes = io.BytesIO()
-                image.save(img_bytes, format='JPEG')
-                img_bytes.seek(0)
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="✅ Image has been successfully uploaded!", width="content")
 
-                # files = {"image": ("image.jpeg", img_bytes, "image/jpeg")}
-                files = {"file": (uploaded_file.name, img_bytes, uploaded_file.type)}   
+    confidence_threshold = st.slider("Confidence Threshold (%)", 
+                                     min_value=0.70, 
+                                     max_value=1.0, 
+                                     value=FINGERPRINT_CONFIDENCE_DEFAULT_THRESHOLD, 
+                                     step=0.01,
+                                     format="percent",
+                                     help="Set the confidence threshold for fingerprint class prediction. Predictions with confidence below this threshold will be considered unreliable.")
 
-                # Send request to FastAPI backend
-                response = requests.post(f"{FINGERPRINT_FASTAPI_URL}/predict_fingerprint", files=files)
+    if st.button("Predict FingerPrint Class", 
+                 type="primary", 
+                 help="Click to predict the fingerprint class (i.e. 'thumb', 'index', 'middle', 'ring', or 'little') of the uploaded image."):
+        if uploaded_file is None:
+            st.warning("⚠️ Please upload an image to predict its fingerprint class.")
+        else:
+            with st.spinner("Predicting fingerprint image..."):
+                try:
+                    # Convert image to bytes
+                    img_bytes = io.BytesIO()
+                    image.save(img_bytes, format='JPEG')
+                    img_bytes.seek(0)
 
-                confidence = response.json().get("confidence", 0)
-                if confidence < confidence_threshold*100:
-                    st.warning(f"⚠️ The confidence level of the prediction is {confidence:.2f}%, which is below the threshold of {confidence_threshold*100:.2f}%.")
-                    st.warning("The fingerprint image may not be actionable.")
-                else:
-                    st.success(f"✅ The confidence level of the prediction is {confidence:.2f}%, which is above the threshold of {confidence_threshold*100:.2f}%.")
-                    st.success("The fingerprint image is likely to be actionable.")
+                    # files = {"image": ("image.jpeg", img_bytes, "image/jpeg")}
+                    files = {"file": (uploaded_file.name, img_bytes, uploaded_file.type)}   
 
-                st.write("Prediction for all fingerprint classes:")
-                st.write(response.json())
+                    # Send request to FastAPI backend
+                    response = requests.post(f"{FINGERPRINT_FASTAPI_URL}/predict_fingerprint", files=files)
 
-            except Exception as e:
-                st.error(f"E⚠️ Unable to predict the fingerprint class from the uploaded image '{uploaded_file.name}': {e}")
+                    confidence = response.json().get("confidence", 0)
+                    if confidence < confidence_threshold*100:
+                        st.warning(f"⚠️ The confidence level of the prediction is {confidence:.2f}%, which is below the threshold of {confidence_threshold*100:.2f}%.")
+                        st.warning("The fingerprint image may not be actionable.")
+                    else:
+                        st.success(f"✅ The confidence level of the prediction is {confidence:.2f}%, which is above the threshold of {confidence_threshold*100:.2f}%.")
+                        st.success("The fingerprint image is likely to be actionable.")
+
+                    st.write("Prediction for all fingerprint classes:")
+                    st.write(response.json())
+
+                except Exception as e:
+                    st.error(f"E⚠️ Unable to predict the fingerprint class from the uploaded image '{uploaded_file.name}': {e}")
+
+# ---------------------------------------------------------------------------------------------
+# Tab for application information and instructions
+# ---------------------------------------------------------------------------------------------
+
+with tabs[1]:
+    st.header("About the FingerPrint Validator Application")
+
+    st.write("""
+        The FingerPrint Validator application is designed to analyze fingerprint images and predict their classes (e.g., thumb, index, middle, ring, little) using a machine learning model hosted on a FastAPI backend. 
+        Users can upload fingerprint images in BMP or JPEG format and receive predictions along with confidence levels. 
+        The application also allows users to set a confidence threshold to determine whether the fingerprint images are actionable.
+    """)
+    st.write("""
+        **How to Use:**
+        1. Upload a fingerprint image by clicking the "Choose an image..." button.
+        2. Adjust the confidence threshold using the slider to set the minimum confidence level for actionable predictions.
+        3. Click the "Predict FingerPrint Class" button to generate predictions for the uploaded image.
+        4. Review the predicted classes and their confidence levels displayed below the prediction button.
+    """)
+    st.write("""
+        **Note:** Ensure that the FastAPI backend is running and accessible at `http://localhost:8000` for the streamlitapplication to function correctly.
+                    The FastAPI backend should have an endpoint `/predict_fingerprint` that accepts image files and returns predictions in JSON format.
+                    The FastAPI backend is responsible for processing the uploaded images, running them through the machine learning model, and returning the predicted classes and confidence levels to the Streamlit frontend for display.
+    """)
+    st.write("""
+        Technology Stack:
+        - Streamlit: for creating the web application interface.
+        - FastAPI: For hosting the machine learning model and handling API requests.
+        - TensorFlow/Keras MobileNetV2: For the machine learning model used in fingerprint class prediction.
+        - Transfer learning and fine tuning training techniques have been carried out on the machine learning model, which consists of:
+             - MobileNetV2 architecture (pre-trained on ImageNet)
+             - Hidden dense layers with 256 and 64 neurons respectively, with Leaky_ReLU activation function
+             - Classification layer with softmax activation function for the 5 fingerprint classes.
+        - Kaggle SOCOFing dataset: for training and evaluating the machine learning model.
+    """)
